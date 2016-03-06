abe8ecb4f082f0145e430e32b9cb05da9e59ff2a
Michael Patraw <michaelpatraw@gmail.com>
2016-03-06 12:12:57 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dimlit
-A possible 2016 7DRL entry.
+
+In Dimlit, a rogue finds herself in a dark field, with a single dwindling light source. The rogue must use resources found around the field to power the light source and construct other structures to help her kill the creature lurking in the dark before it's too late.
+
+# Building
+
+You will need Make, and [termbox](https://github.com/nsf/termbox) installed onto your system then run the following commands:
+
+```
+cd build
+make
+../bin/dimlit
+```
---
+++ build/premake5.lua
@@ -0,0 +1,40 @@
+newoption {
+    trigger     = "test",
+    description = "build tests"
+}
+
+workspace "dimlit"
+    configurations {"Debug", "Release"}
+    language "C++"
+    buildoptions {"-std=c++14"}
+    toolset "clang"
+
+filter "configurations:Debug"
+    defines {"DEBUG"}
+    flags {"Symbols", "FatalWarnings"}
+    buildoptions {"-Wall", "-Wextra", "-Wpedantic"}
+
+filter "configurations:Release"
+    defines {"NDEBUG"}
+    optimize "On"
+
+project "dimlit-lib"
+    kind "StaticLib"
+    files {"../src/**.cpp"}
+    removefiles "../src/main.cpp"
+    includedirs {"/usr/local/include"}
+
+project "dimlit"
+    kind "ConsoleApp"
+    files "../src/main.cpp"
+    includedirs {"/usr/local/include"}
+    libdirs {"/usr/local/lib"}
+    links {"dimlit-lib", "termbox"}
+    targetdir "../bin"
+
+if _OPTIONS["test"] then
+    project "dimlit-test"
+        kind "ConsoleApp"
+        files "../test/main.cpp"
+        links {"dimlit-lib"}
+end
